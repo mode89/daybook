fef14b9277c3dca797b930cc80a12f2ddb29ce8b
Mode <mode89@mail.ru>
2017-03-30 18:07:03 +0800

    remove features/environment.py

--- features/environment.py
+++
@@ -1,7 +0,0 @@
-from unittest import mock
-
-def after_step(context, step):
-    if context.feature.name == "Core functionality" and \
-        context.scenario.name == "edit text" and \
-        step.name == "application":
-        context.application.edit = mock.Mock(return_value=str())
