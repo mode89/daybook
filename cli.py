84ccc40127ca1747936fee0cde634e8ae9eace2d
Mode <mode89@mail.ru>
2017-03-31 11:50:13 +0800

    define main function

--- cli.py
+++ cli.py
@@ -2,8 +2,11 @@ import application
 import os
 import sys
 
-if __name__ == "__main__":
+def main():
     app = application.Application()
     app.config_path = os.path.expanduser("~/.journal")
     app.args = sys.argv[1:]
     app.run()
+
+if __name__ == "__main__":
+    main()
