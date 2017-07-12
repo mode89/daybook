a02da1b668212247189124bc7a17e4baf2e40e1c
Mode <mode89@mail.ru>
2017-07-13 00:25:32 +0800

    keep text and binary representations of journal

--- daybook/journal.py
+++ daybook/journal.py
@@ -5,22 +5,29 @@ class Journal:
     def __init__(self):
         self.path = str()
         self.text = str()
+        self.data = bytes()
 
     def load(self):
-        with open(self.path, "r") as f:
-            self.text = f.read()
+        with open(self.path, "rb") as f:
+            self.data = f.read()
 
     def save(self):
-        with open(self.path, "w") as f:
-            f.write(self.text)
+        with open(self.path, "wb") as f:
+            f.write(self.data)
+
+    def decode(self):
+        self.text = self.data.decode("utf-8")
+
+    def encode(self):
+        self.data = self.text.encode("utf-8")
 
     def append(self, entry):
         self.text += str(entry) + "\n"
 
     def encrypt(self):
         cipher = daybook.cipher.Cipher(self.password)
-        self.text = cipher.encrypt(self.text.encode("utf-8"))
+        self.data = cipher.encrypt(self.data)
 
     def decrypt(self):
         cipher = daybook.cipher.Cipher(self.password)
-        self.text = cipher.decrypt(self.text).decode("utf-8")
+        self.data = cipher.decrypt(self.data)
